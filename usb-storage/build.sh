44b5f5db278bdac0c7702402ade58411bb34ae05
Lukas F. Hartmann <lukas@mntre.com>
2021-09-25 16:48:49 +0200

    usb: tune for 020-060

--- usb-storage/build.sh
+++ usb-storage/build.sh
@@ -1,8 +1,7 @@
 export PATH=$PATH:/opt/amiga/bin
 
-m68k-amigaos-gcc -m68000 -s -Wall -Wextra -Wno-unused-parameter -fomit-frame-pointer -nostdlib -nostartfiles -O2 -o zzusb.device mntsd_device.c mntsd_cmd.c rdb_partitions.c
+m68k-amigaos-gcc -m68020 -mtune=68020-60 -s -Wall -Wextra -Wno-unused-parameter -fomit-frame-pointer -nostdlib -nostartfiles -O2 -o zzusb.device mntsd_device.c mntsd_cmd.c rdb_partitions.c
 
 xxd -i -a zzusb.device > zzusb-device.h
 
 cp zzusb-device.h ~/code/ZZ9000_proto/ZZ9000_proto.sdk/ZZ9000OS/src/
-
