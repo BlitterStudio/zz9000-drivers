decc990c407aac432999a19017974b0ac5a53ad7
Lukas F. Hartmann <lukas@mntre.com>
2022-05-03 21:25:19 +0200

    ahi: add -ldebug

--- ahi/driver/build.sh
+++ ahi/driver/build.sh
@@ -7,4 +7,4 @@ vasmm68k_mot -quiet -phxass -Fhunk -m68020 -o PREFSFILE.uncut prefsfile.a -I/opt
 # remove 0x28 bytes from the start
 dd bs=1 skip=40 if=PREFSFILE.uncut of=ZZ9000AX
 
-m68k-amigaos-gcc zz9000ax-ahi.c asmfuncs.s -O3 -o zz9000ax.audio -Wall -Wextra -Wno-unused-parameter -nostartfiles -m68020
+m68k-amigaos-gcc zz9000ax-ahi.c asmfuncs.s -O3 -o zz9000ax.audio -Wall -Wextra -Wno-unused-parameter -nostartfiles -m68020 -ldebug
